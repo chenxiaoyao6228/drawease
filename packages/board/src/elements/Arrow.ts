b268add53824c23de105dc4d52beb178fd85e425
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2024-04-16 22:00:10 +0800

    feat: 添加Arrow类型

--- packages/board/src/elements/Arrow.ts
+++ packages/board/src/elements/Arrow.ts
@@ -9,8 +9,29 @@ export default class ArrowElement extends BaseElement {
   render(renderConfig: RenderConfig) {
     const { rc, ctx } = renderConfig;
 
-    const { points, strokeColor, strokeWidth, roughness = 1 } = this._data as ArrowElementData;
+    const { points, x, y, strokeColor, fillStyle, strokeWidth, roughness = 1 } = this._data as ArrowElementData;
 
-    //TODO
+    ctx.save();
+    ctx.translate(x, y);
+
+    // 绘制箭头线段
+    const [startX, startY] = points[0];
+    const [endX, endY] = points[1];
+    rc.line(startX, startY, endX, endY, { stroke: strokeColor, strokeWidth, roughness });
+
+    // 绘制箭头头部
+    const dx = endX - startX;
+    const dy = endY - startY;
+    const arrowHeadSize = 30;
+    const arrowAngle = 15;
+    const angleRadians = Math.atan2(dy, dx);
+    const leftAngle = angleRadians - Math.PI / (180 / arrowAngle);
+    const rightAngle = angleRadians + Math.PI / (180 / arrowAngle);
+    const leftEndX = endX - arrowHeadSize * Math.cos(leftAngle);
+    const leftEndY = endY - arrowHeadSize * Math.sin(leftAngle);
+    const rightEndX = endX - arrowHeadSize * Math.cos(rightAngle);
+    const rightEndY = endY - arrowHeadSize * Math.sin(rightAngle);
+    rc.line(endX, endY, leftEndX, leftEndY, { stroke: strokeColor, strokeWidth, roughness });
+    rc.line(endX, endY, rightEndX, rightEndY, { stroke: strokeColor, strokeWidth, roughness });
   }
 }
